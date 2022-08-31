c0784eacab6daacb57b73b727247a70c3c5a2f1e
yangchou19 <49429748+yangchou19@users.noreply.github.com>
2022-08-31 14:51:36 +0800

    object类型修改

--- src/main/java/Send.java
+++ src/main/java/Send.java
@@ -1,13 +1,13 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.omg.CORBA.Object;
 
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.lang.Object;
 
 import static java.lang.Thread.sleep;
 
--- src/main/java/ZcashNet.java
+++ src/main/java/ZcashNet.java
@@ -1,7 +1,6 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import org.omg.CORBA.Object;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -13,6 +12,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.lang.Object;
 
 import static java.lang.Thread.sleep;
 
