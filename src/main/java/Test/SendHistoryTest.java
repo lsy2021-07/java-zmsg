141d6218ad445309387bdec1c12a25c8315f92d2
lsy2021-07 <1071lsy@shu.edu.cn>
2022-09-06 15:32:14 +0800

    发送历史接口

---
+++ src/main/java/Test/SendHistoryTest.java
@@ -0,0 +1,23 @@
+package Test;
+
+import org.junit.Test;
+import zcash.Receive;
+import zcash.SendHistory;
+
+import java.util.HashMap;
+
+public class SendHistoryTest {
+    String serviceIp = "8.219.9.193";
+
+    @Test
+    public void sendHistoryTest() {
+
+//        HashMap result = new Receive().checkMessage(serviceIp,
+//                "zs1h4mx4nt5m3pdqtwg3x9mu9e7wgpuyj2qjp7jf7l0cnjeh0gcmmcsz5vp79w6s5vraza677fsvdp");
+        HashMap result = new SendHistory().GetSendHistory(serviceIp, "zs1h4mx4nt5m3pdqtwg3x9mu9e7wgpuyj2qjp7jf7l0cnjeh0gcmmcsz5vp79w6s5vraza677fsvdp");
+//        HashMap result = new Receive().GetReceiveHistory(serviceIp,
+//                "zs1w9sk86zx980lu0e30zm8t9xp3cjadhwnel70qyhtzmawuppjq66kmmmkv409eg90sk4jxdymjwg");
+        System.out.println(result);
+    }
+}
+
--- src/main/java/zcash/ZcashProxy.java
+++ src/main/java/zcash/ZcashProxy.java
@@ -10,6 +10,7 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class ZcashProxy {
@@ -164,5 +165,32 @@ public class ZcashProxy {
         System.out.println(mapResult);
         return mapResult;
     }
+
+    public static String hex_decode(String s_hex) {
+        int index = 0;
+        for (int i = s_hex.length() - 1; i >= 0; i--) {
+            if (s_hex.charAt(i)!='0'){
+                index = i;
+                break;
+            }
+        }
+        s_hex = s_hex.substring(0, index + 1);
+        if (s_hex.equals("f6") || s_hex.length()%2 != 0){
+            return "";
+        }
+        StringBuilder s_acsii = new StringBuilder();
+        for (int i = 0; i < s_hex.length(); i+=2){
+            String str = s_hex.substring(i, i+2);
+            s_acsii.append((char)Integer.parseInt(str,16));
+        }
+        return s_acsii.toString();
+    }
+
+    public String unixtimeToData(String time){
+        time += "000";   // python time stamp to java time stamp
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return  simpleDateFormat.format(new Date(new Long(time)));
+    }
+
 
 }
