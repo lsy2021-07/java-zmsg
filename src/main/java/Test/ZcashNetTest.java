ad5eb89e758c4cf48cb34622f12f262fd326242b
yangchou19 <49429748+yangchou19@users.noreply.github.com>
2022-09-11 20:44:25 +0800

    返回状态修改

--- src/main/java/Test/ZcashNetTest.java
+++ src/main/java/Test/ZcashNetTest.java
@@ -32,4 +32,11 @@ public class ZcashNetTest {
                 "zs1h4mx4nt5m3pdqtwg3x9mu9e7wgpuyj2qjp7jf7l0cnjeh0gcmmcsz5vp79w6s5vraza677fsvdp");
         System.out.println(result);
     }
+
+    @Test
+    public void getAllAddress() {
+        HashMap result = zcashNet.getAllAddress(serviceIp, id);
+        System.out.println(result);
+    }
+
 }
